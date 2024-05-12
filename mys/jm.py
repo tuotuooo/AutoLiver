15c92a046cfa132da18b6ce8ae9e6a040f901746
Tuo_Tuo <74456138+tuotuooo@users.noreply.github.com>
2024-05-12 19:12:51 +0800

    Add files via upload

---
+++ mys/jm.py
@@ -0,0 +1,42 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+import base64
+import configparser
+
+def jm(z,m):
+    with open("1.pem", "rb") as key_file:
+        pem_public_key = key_file.read()
+    public_key = serialization.load_pem_public_key(pem_public_key)
+
+    message_bytes = z.encode('utf-8')
+    message_bytes1 = m.encode('utf-8')
+
+    ciphertext = public_key.encrypt(
+        message_bytes,
+        padding.PKCS1v15()
+    )
+    ciphertext1 = public_key.encrypt(
+        message_bytes1,
+        padding.PKCS1v15()
+    )
+
+    base64_encoded = base64.b64encode(ciphertext)
+    base64_encoded1 = base64.b64encode(ciphertext1)
+
+    return base64_encoded.decode(),base64_encoded1.decode()
+
+def main():
+    canshu = {}
+    config = configparser.ConfigParser()
+    config.read('canshu.cfg')
+    z = config['mw']['account']
+    m = config['mw']['password']
+    canshu['account'] = jm(z,m)[0]
+    canshu['password'] = jm(z,m)[1]
+    for key, value in canshu.items():
+        config.set('credentials', key, value)
+    with open('canshu.cfg', 'w') as configfile:
+        config.write(configfile)
+
+if __name__ == "__main__":
+    main()
---
+++ mys/qd_requests.py
@@ -0,0 +1,51 @@
+import requests
+import json
+import configparser
+import sys
+
+def qiandaorequest(headers,game_uid,url,hdid,region):
+    data = {
+        "act_id": hdid,
+        "region": region,
+        "uid": game_uid,
+        "lang": "zh-cn"
+    }
+    response1 = requests.post(url, headers=headers, data=json.dumps(data))
+    data1 = response1.text
+    parsed_data1 = json.loads(data1)
+    try:
+        if parsed_data1["data"]["gt"] == "":
+            return data1
+        else:
+            challenge = parsed_data1['data']['challenge']
+            headers['x-rpc-challenge'] = challenge
+            response = requests.post(url, headers=headers, data=json.dumps(data))
+            return response.text
+    except:
+        return data1
+
+def main():
+    game_uid = sys.argv[1]
+    hdid = sys.argv[2]
+    region = sys.argv[3]
+    dh = sys.argv[4]
+    config = configparser.ConfigParser()
+    config.read('canshu.cfg')
+    login_ticket = config['cs']['login_ticket']
+    ltoken = config['cs']['ltoken']
+    cookie_token = config['cs']['cookie_token']
+    mys_id = config['yx']['mys_id']
+    url = config['api']['url']
+
+    headers = {
+        'x-rpc-signgame': dh,
+        'Cookie':
+            'login_ticket=' + login_ticket + ';' + \
+            'account_id=' + mys_id + ';' + \
+            'ltoken=' + ltoken + ';' + \
+            'cookie_token=' + cookie_token + ';'
+    }
+    print(qiandaorequest(headers,game_uid,url,hdid,region))
+
+if __name__ == "__main__":
+    main()
