ddde14b4d6d02a404f3440a54071e0d6b4d8c86a
Tuo_Tuo <74456138+tuotuooo@users.noreply.github.com>
2025-01-16 17:10:46 +0800

    Update and rename request_vfscp.py to request_vfscp_sm.py

--- wyds/request_vfscp_sm.py
+++ wyds/request_vfscp_sm.py
@@ -3,8 +3,8 @@ import json
 import configparser
 import subprocess
 
-def requesturl(y):
-    url = 'https://dl.reg.163.com/dl/wxdl/yd/vfscp'
+def requesturl(x, y):
+    url = x
 
     data = json.dumps({
         'encParams': y
@@ -17,7 +17,7 @@ def requesturl(y):
         'Connection': 'keep-alive',
         'Content-Type': 'application/json',
         'Host': 'dl.reg.163.com',
-        'Referer': 'https://servicewechat.com/wx53eacbe0d8a7a95a/186/page-frame.html',
+        'Referer': 'https://servicewechat.com/wx53eacbe0d8a7a95a/188/page-frame.html',
         'Sec-Fetch-Dest': 'empty',
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'cross-site',
@@ -29,6 +29,8 @@ def requesturl(y):
     return response.text
 
 def main():
+    x = 'https://dl.reg.163.com/dl/wxdl/yd/vfscp'
+    w = 'https://dl.reg.163.com/dl/wxdl/dlzc/yd/sms/sm'
     config = configparser.ConfigParser()
     config.read('canshu.cfg',encoding='utf-8')
     id = config['ini']['id']
@@ -38,8 +40,12 @@ def main():
     cap = result.stdout.replace("\n", "")
     xxx = '{"pd":"godlike_wyds_xcx","pkid":"VYoLaWc","pkht":"ds.163.com","channel":102,"id":"' +id+'","cap":"'+cap+'","un":"'+ppp+'"}'
     result = subprocess.run(['node', 'sm4jm.js', str(xxx)], capture_output=True, text=True)
+    output2 = result.stdout.replace("\n", "")
+    r = requesturl(x, output2)
+    sss = '{"pd":"godlike_wyds_xcx","pkid":"VYoLaWc","pkht":"ds.163.com","channel":102,"id":"' + id + '","un":"' + ppp + '"}'
+    result = subprocess.run(['node', 'sm4jm.js', str(sss)], capture_output=True, text=True)
     output = result.stdout.replace("\n", "")
-    print(requesturl(output))
-
+    f = requesturl(w, output)
+    print(f)
 if __name__ == "__main__":
     main()
